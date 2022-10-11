c7857edd58563ee1d0e35736ba45d1c76f8e5e40
MariNafikova <nafikova2708@mail.ru>
2022-10-12 00:08:49 +0300

    Редактирование чата
    - Доработано 2-е дз;
    - Переделана форма с использованием компонентов из material-ui.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import {useEffect, useState} from "react";
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 function App() {
   const [messageList, setMessageList] = useState([]);
@@ -19,26 +24,77 @@ function App() {
       ]
     );
   }
-  // useEffect(() => {
-  //   setTimeout(,1500)
-  // }, messageList)
+
+  function botAnswer() {
+    const lastAuthor = messageList[messageList.length - 1];
+    if (lastAuthor && lastAuthor.author && lastAuthor.author !== "bot") {
+      setMessageList(stateList => [
+        ...stateList, {
+          id: uuidv4(),
+          text: `Сообщение автора ${lastAuthor.author} отправлено`,
+          author: "bot"
+        }
+      ])
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      botAnswer()
+    }, 500)
+  }, [messageList])
+
   return (
     <div className="main">
       <form onSubmit={handleSubmit} className="form">
-        <input className="formInput" placeholder="Введите имя" type="text" name="author"></input> <br/>
-        <input className="formInput" placeholder="Введите сообщение" type="text" name="text"></input> <br/>
-        <input className="formButton" type="submit" value="Отправить"/>
+          <TextField id="outlined-basic"
+                     label="Имя"
+                     variant="outlined"
+                     placeholder="Введите имя"
+                     type="text"
+                     name="author"
+                     size="small"
+                     color="secondary" />
+      <br/>
+        {/*<input className="formInput" placeholder="Введите сообщение" type="text" name="text"></input> <br/>*/}
+          <TextField id="outlined-basic"
+                     label="Сообщение"
+                     variant="outlined"
+                     placeholder="Введите сообщение"
+                     type="text"
+                     name="text"
+                     size="small"
+                     color="secondary" />
+      <br/>
+        <Stack direction="row" spacing={1}>
+          <Button size="small" variant="contained" color="secondary" type="submit" value="Отправить">
+            Отправить
+          </Button>
+        </Stack>
+        {/*<input className="formButton" type="submit" value="Отправить"/>*/}
       </form>
       {messageList.map((message) => {
-        return (
-          <div key={message.id} className="answerBlock">
-            <p>Сообщение: "{message.text}"</p>
-            <p>Автор: {message.author} </p>
-          </div>
-        )
+        if (message.author !== "bot") {
+          return (
+            <div key={message.id} className="answerBlock">
+              <p>Сообщение: "{message.text}"</p>
+              <p>Автор: {message.author} </p>
+            </div>
+          )
+        } else {
+          return (
+            <div key={message.id} className="answerBlock">
+              <p>"{message.text}"</p>
+            </div>
+          )
+        }
       })
       }
     </div>
   )
 }
+
 export default App;
+
+
+
