73d634de6e429e591637dfe9a46934c92cf5a77f
MariNafikova <nafikova2708@mail.ru>
2022-10-17 21:54:25 +0300

    Редактирование чата
    - Доработано 3-е дз;
    - Добавен список чатов с использованием List и ListItem из material-ui.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import {v4 as uuidv4} from 'uuid';
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
+import SendIcon from '@mui/icons-material/Send';
 import TextField from '@mui/material/TextField';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+
 
 function App() {
   const [messageList, setMessageList] = useState([]);
@@ -44,52 +52,93 @@ function App() {
     }, 500)
   }, [messageList])
 
+  const [chatsList, setChatsList] = useState([
+    {id: uuidv4(), name: 'Andrew', avatar: 'https://mui.com/static/images/avatar/1.jpg', topic: 'Monday meeting', messageText: ' — Agreed! Meet me at 3 p.m...'},
+    {id: uuidv4(), name: 'Olga', avatar: 'https://mui.com/static/images/avatar/3.jpg', topic: 'Theatre tickets', messageText:' — There are many options for theatre performance for you...'},
+    {id: uuidv4(), name: 'Vladimir', avatar: 'https://mui.com/static/images/avatar/2.jpg', topic: 'Excursion trip', messageText:' — The bus departs at 7 a.m. every day...'},
+  ]);
+
   return (
     <div className="main">
-      <form onSubmit={handleSubmit} className="form">
-          <TextField id="outlined-basic"
-                     label="Имя"
-                     variant="outlined"
-                     placeholder="Введите имя"
+      <div className="chatsField">
+        <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
+          {chatsList.map((chat) => {
+            return (
+              <ListItem  key={chatsList.id} alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt={chat.name} src={chat.avatar}/>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={chat.topic}
+                  secondary={
+                    <React.Fragment>
+                      <Typography
+                        sx={{display: 'inline'}}
+                        component="span"
+                        variant="body2"
+                        color="text.primary"
+                      >
+                        {chat.name}
+                      </Typography>
+                      {chat.messageText}
+                    </React.Fragment>
+                  }
+                />
+              </ListItem>
+            );
+          })}
+        </List>
+      </div>
+      <div className="formField">
+        <form onSubmit={handleSubmit} className="form">
+          <TextField id="standard-basic"
+                     label="Name"
+                     variant="standard"
+                     placeholder="Enter your name"
                      type="text"
                      name="author"
                      size="small"
-                     color="secondary" />
-      <br/>
-        {/*<input className="formInput" placeholder="Введите сообщение" type="text" name="text"></input> <br/>*/}
-          <TextField id="outlined-basic"
-                     label="Сообщение"
-                     variant="outlined"
-                     placeholder="Введите сообщение"
-                     type="text"
-                     name="text"
-                     size="small"
-                     color="secondary" />
-      <br/>
-        <Stack direction="row" spacing={1}>
-          <Button size="small" variant="contained" color="secondary" type="submit" value="Отправить">
-            Отправить
-          </Button>
-        </Stack>
-        {/*<input className="formButton" type="submit" value="Отправить"/>*/}
-      </form>
-      {messageList.map((message) => {
-        if (message.author !== "bot") {
-          return (
-            <div key={message.id} className="answerBlock">
-              <p>Сообщение: "{message.text}"</p>
-              <p>Автор: {message.author} </p>
-            </div>
-          )
-        } else {
-          return (
-            <div key={message.id} className="answerBlock">
-              <p>"{message.text}"</p>
-            </div>
-          )
+                     color="secondary"
+                     autoFocus={true}/>
+          <br/>
+          {/*<input className="formInput" placeholder="Введите сообщение" type="text" name="text"></input> <br/>*/}
+          <div className="messageBlock">
+            <TextField id="outlined-basic"
+                       label="Message"
+                       variant="outlined"
+                       placeholder="Enter a message"
+                       type="text"
+                       name="text"
+                       size="big"
+                       color="secondary"
+            />
+            <Stack direction="row" spacing={1}>
+              <Button size="small" variant="contained" color="secondary" type="submit" value="Send"
+                      endIcon={<SendIcon/>}>
+                Send
+              </Button>
+            </Stack>
+            {/*<input className="formButton" type="submit" value="Отправить"/>*/}
+          </div>
+        </form>
+        {messageList.map((message) => {
+          if (message.author !== "bot") {
+            return (
+              <div key={message.id} className="answerBlock">
+                <p>Message: "{message.text}"</p>
+                <p>Author: {message.author} </p>
+              </div>
+            )
+          } else {
+            return (
+              <div key={message.id} className="answerBlock">
+                <p>"{message.text}"</p>
+              </div>
+            )
+          }
+        })
         }
-      })
-      }
+      </div>
     </div>
   )
 }
